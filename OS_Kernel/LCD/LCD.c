617866c34a7eef6b6cfb4ea16abd52715a51280c
tuzi yang <1027235007@qq.com>
2019-03-13 10:48:47 +0800

    初始化LCD增加基本函数

--- OS_Kernel/LCD/LCD.c
+++ OS_Kernel/LCD/LCD.c
@@ -4,10 +4,257 @@
 #include "stm32f2xx.h"
 #include "tool.h"
 
-void putchar()
+/***********************************初始化LCD屏幕**********************************/
+void init_LCD()
 {
-	
+	/* Enable GPIOs clock */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD | RCC_AHB1Periph_GPIOE | RCC_AHB1Periph_GPIOC, ENABLE);
+
+	/* Enable FSMC clock */
+	RCC_AHB3PeriphClockCmd(RCC_AHB3Periph_FSMC, ENABLE);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;				 //PC0推挽输出 
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT; 		 //推挽谁出
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	GPIO_SetBits(GPIOD, GPIO_Pin_12);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;				 //PC0 推挽输出 背光
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT; 		 //推挽输出
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	GPIO_SetBits(GPIOC, GPIO_Pin_0);
+
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource0, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource1, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource4, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource5, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource7, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource8, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource9, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource10, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource11, GPIO_AF_FSMC);
+	//GPIO_PinAFConfig(GPIOD, GPIO_PinSource12, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource14, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource15, GPIO_AF_FSMC);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_7 |
+		GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_14 | GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_25MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+	/* GPIOE configuration */
+	GPIO_PinAFConfig(GPIOE, GPIO_PinSource7, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOE, GPIO_PinSource8, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOE, GPIO_PinSource9, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOE, GPIO_PinSource10, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOE, GPIO_PinSource11, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOE, GPIO_PinSource12, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOE, GPIO_PinSource13, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOE, GPIO_PinSource14, GPIO_AF_FSMC);
+	GPIO_PinAFConfig(GPIOE, GPIO_PinSource15, GPIO_AF_FSMC);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11 |
+		GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+
+	GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+	/*-- FSMC Configuration ------------------------------------------------------*/
+	p.FSMC_AddressSetupTime = 6;
+	p.FSMC_AddressHoldTime = 1;
+	p.FSMC_DataSetupTime = 9;
+	p.FSMC_BusTurnAroundDuration = 1;
+	p.FSMC_CLKDivision = 0;
+	p.FSMC_DataLatency = 0;
+	p.FSMC_AccessMode = FSMC_AccessMode_A;
+
+	FSMC_NORSRAMInitStructure.FSMC_Bank = FSMC_Bank1_NORSRAM1;
+	FSMC_NORSRAMInitStructure.FSMC_DataAddressMux = FSMC_DataAddressMux_Disable;
+	FSMC_NORSRAMInitStructure.FSMC_MemoryType = FSMC_MemoryType_SRAM;
+	FSMC_NORSRAMInitStructure.FSMC_MemoryDataWidth = FSMC_MemoryDataWidth_16b;
+	FSMC_NORSRAMInitStructure.FSMC_BurstAccessMode = FSMC_BurstAccessMode_Disable;
+	FSMC_NORSRAMInitStructure.FSMC_AsynchronousWait = FSMC_AsynchronousWait_Disable;
+	FSMC_NORSRAMInitStructure.FSMC_WaitSignalPolarity = FSMC_WaitSignalPolarity_Low;
+	FSMC_NORSRAMInitStructure.FSMC_WrapMode = FSMC_WrapMode_Disable;
+	FSMC_NORSRAMInitStructure.FSMC_WaitSignalActive = FSMC_WaitSignalActive_BeforeWaitState;
+	FSMC_NORSRAMInitStructure.FSMC_WriteOperation = FSMC_WriteOperation_Enable;
+	FSMC_NORSRAMInitStructure.FSMC_WaitSignal = FSMC_WaitSignal_Disable;
+	FSMC_NORSRAMInitStructure.FSMC_ExtendedMode = FSMC_ExtendedMode_Disable;
+	FSMC_NORSRAMInitStructure.FSMC_WriteBurst = FSMC_WriteBurst_Disable;
+	FSMC_NORSRAMInitStructure.FSMC_ReadWriteTimingStruct = &p;
+	FSMC_NORSRAMInitStructure.FSMC_WriteTimingStruct = &p;
+
+	FSMC_NORSRAMInit(&FSMC_NORSRAMInitStructure);
+
+	/*!< Enable FSMC Bank1_SRAM1 Bank */
+	FSMC_NORSRAMCmd(FSMC_Bank1_NORSRAM1, ENABLE);
+	Delay_ms(200);
+	GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+	Delay_ms(500);
+	GPIO_SetBits(GPIOD, GPIO_Pin_12);
+	Delay_ms(500);
+	Delay_ms(50); 					// delay 50 ms 
+	LCD_IO_WriteReg(0xCF);  //这段代码是用来设置LCD横屏竖屏显示方式，即x,y坐标
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteData(0xC1);
+	LCD_IO_WriteData(0X30);
+	LCD_IO_WriteReg(0xED);
+	LCD_IO_WriteData(0x64);
+	LCD_IO_WriteData(0x03);
+	LCD_IO_WriteData(0X12);
+	LCD_IO_WriteData(0X81);
+	LCD_IO_WriteReg(0xE8);
+	LCD_IO_WriteData(0x85);
+	LCD_IO_WriteData(0x10);
+	LCD_IO_WriteData(0x7A);
+	LCD_IO_WriteReg(0xCB);
+	LCD_IO_WriteData(0x39);
+	LCD_IO_WriteData(0x2C);
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteData(0x34);
+	LCD_IO_WriteData(0x02);
+	LCD_IO_WriteReg(0xF7);
+	LCD_IO_WriteData(0x20);
+	LCD_IO_WriteReg(0xEA);
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteReg(0xC0);    //Power control 
+	LCD_IO_WriteData(0x1B);   //VRH[5:0] 
+	LCD_IO_WriteReg(0xC1);    //Power control 
+	LCD_IO_WriteData(0x01);   //SAP[2:0];BT[3:0] 
+	LCD_IO_WriteReg(0xC5);    //VCM control 
+	LCD_IO_WriteData(0x30); 	 //3F
+	LCD_IO_WriteData(0x30); 	 //3C
+	LCD_IO_WriteReg(0xC7);    //VCM control2 
+	LCD_IO_WriteData(0XB7);
+	LCD_IO_WriteReg(0x36);    // Memory Access Control 
+	LCD_IO_WriteData(0x48);
+	LCD_IO_WriteReg(0x3A);
+	LCD_IO_WriteData(0x55);
+	LCD_IO_WriteReg(0xB1);
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteData(0x1A);
+	LCD_IO_WriteReg(0xB6);    // Display Function Control 
+	LCD_IO_WriteData(0x0A);
+	LCD_IO_WriteData(0xA2);
+	LCD_IO_WriteReg(0xF2);    // 3Gamma Function Disable 
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteReg(0x26);    //Gamma curve selected 
+	LCD_IO_WriteData(0x01);
+	LCD_IO_WriteReg(0xE0);    //Set Gamma 
+	LCD_IO_WriteData(0x0F);
+	LCD_IO_WriteData(0x2A);
+	LCD_IO_WriteData(0x28);
+	LCD_IO_WriteData(0x08);
+	LCD_IO_WriteData(0x0E);
+	LCD_IO_WriteData(0x08);
+	LCD_IO_WriteData(0x54);
+	LCD_IO_WriteData(0XA9);
+	LCD_IO_WriteData(0x43);
+	LCD_IO_WriteData(0x0A);
+	LCD_IO_WriteData(0x0F);
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteReg(0XE1);    //Set Gamma 
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteData(0x15);
+	LCD_IO_WriteData(0x17);
+	LCD_IO_WriteData(0x07);
+	LCD_IO_WriteData(0x11);
+	LCD_IO_WriteData(0x06);
+	LCD_IO_WriteData(0x2B);
+	LCD_IO_WriteData(0x56);
+	LCD_IO_WriteData(0x3C);
+	LCD_IO_WriteData(0x05);
+	LCD_IO_WriteData(0x10);
+	LCD_IO_WriteData(0x0F);
+	LCD_IO_WriteData(0x3F);
+	LCD_IO_WriteData(0x3F);
+	LCD_IO_WriteData(0x0F);
+	LCD_IO_WriteReg(0x2B);
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteData(0x01);
+	LCD_IO_WriteData(0x3f);
+	LCD_IO_WriteReg(0x2A);
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteData(0x00);
+	LCD_IO_WriteData(0xef);
+	LCD_IO_WriteReg(0x11); //Exit Sleep
+	Delay_ms(120);
+	LCD_IO_WriteReg(0x29); //display on
+	/********************************************************************************/
+
+	LCD_Color_Fill(0, 0, 239, 159, WHITE);//控制LCD屏幕显示
+	LCD_Color_Fill(0, 160, 239, 319, BLACK);//这里显示 一半白 一半黑
+	LCD_DrawPoint(100, 80, BLUE);          //打一个蓝色的点
 }
+
+/***********************************粗延时函数*************************************/
+vu16 Delay_ms(vu16 Count)
+{
+	u16 i = 0;
+	while (Count--)
+	{
+		for (i = 12000; i > 0; i--);
+	}
+	return Count;
+}
+/***********************************写数据函数**************************************/
+void LCD_IO_WriteReg(uint8_t Reg)
+{
+	/* Write 16-bit Index, then Write Reg */
+	*(__IO uint16_t *)FSMC_LCD_REG = Reg;
+}
+/***********************************设置光标函数*************************************/
+void LCD_IO_WriteData(uint16_t Data)
+{
+	/* Write 16-bit Reg */
+	*(__IO uint16_t *)FSMC_LCD_DATA = Data;
+}
+/***********************************设置光标函数*************************************/
+void LCD_SetCursor(uint16_t Xpos, uint16_t Ypos)
+{
+	LCD_IO_WriteReg(0x2A);
+	LCD_IO_WriteData(Xpos >> 8);
+	LCD_IO_WriteData(Xpos & 0XFF);
+
+	LCD_IO_WriteReg(0x2B);
+	LCD_IO_WriteData(Ypos >> 8);
+	LCD_IO_WriteData(Ypos & 0XFF);
+}
+/***********************************打点函数*************************************/
+void LCD_DrawPoint(u16 x, u16 y, uint16_t GRB)
+{
+	LCD_SetCursor(x, y);
+	LCD_IO_WriteReg(0x2C);
+	LCD_IO_WriteData(GRB);
+}
+/***********************************全屏变色函数*************************************/
+
+void LCD_Color_Fill(uint16_t sx, uint16_t sy, uint16_t ex, uint16_t ey, uint16_t GRB)
+{
+	uint16_t height, width;
+	uint16_t i, j;
+	width = ex - sx + 1;
+	height = ey - sy + 1;
+	for (i = 0; i < height; i++)
+	{
+
+		LCD_IO_WriteReg(0x2C);
+		for (j = 0; j < width; j++)LCD_IO_WriteData(GRB);
+	}
+}
+
 
 
 #endif // !_LCD_H
--- OS_Kernel/LCD/LCD.h
+++ OS_Kernel/LCD/LCD.h
@@ -3,6 +3,33 @@
 
 #include "stm32f2xx.h"
 
+/*
+画笔颜色
+*/
+#define WHITE         	 0xFFFF
+#define BLACK         	 0x0000	  
+#define BLUE         	 0x001F  
+#define BRED             0XF81F
+#define GRED 			 0XFFE0
+#define GBLUE			 0X07FF
+#define RED           	 0xF800
+#define MAGENTA       	 0xF81F
+#define GREEN         	 0x07E0
+#define CYAN          	 0x7FFF
+#define YELLOW        	 0xFFE0
+#define BROWN 			 0XBC40 //棕色
+#define BRRED 			 0XFC07 //棕红色
+#define GRAY  			 0X8430 //灰色
+
+/***********************************函数声明*************************************/
+void init_LCD();
+vu16 Delay_ms(vu16 count);
+void LCD_IO_WriteReg(uint8_t Reg);
+void LCD_IO_WriteData(uint16_t Data);
+void LCD_DrawPoint(u16 x, u16 y, uint16_t GRB);
+void LCD_SetCursor(uint16_t Xpos, uint16_t Ypos);
+void LCD_Color_Fill(uint16_t sx, uint16_t sy, uint16_t ex, uint16_t ey, uint16_t GRB);
+
 
 
 #endif // !_LCD_H
